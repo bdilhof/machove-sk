c48853beea2014a222fc617153efabf5a1462097
Bruno Dilhof <bruno.dilhof@vermont.eu>
2021-06-03 23:59:24 +0200

    Add files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,58 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c28c231414021ad4b42269a185d583933d6b8072',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c28c231414021ad4b42269a185d583933d6b8072',
+    ),
+    'johnpbloch/wordpress' => 
+    array (
+      'pretty_version' => '5.7.2',
+      'version' => '5.7.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'be44aeabc63a584a4b1ef4a193f36c3f710a162a',
+    ),
+    'johnpbloch/wordpress-core' => 
+    array (
+      'pretty_version' => '5.7.2',
+      'version' => '5.7.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd0f5bdeb81800b6bcefee723ebf928b5677e2e86',
+    ),
+    'johnpbloch/wordpress-core-installer' => 
+    array (
+      'pretty_version' => '2.0.0',
+      'version' => '2.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '237faae9a60a4a2e1d45dce1a5836ffa616de63e',
+    ),
+    'wordpress/core-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '5.7.2',
+      ),
+    ),
+  ),
+);
